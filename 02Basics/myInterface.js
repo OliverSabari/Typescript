9dedff1ca7157fc447b5bd31bdb3119330220b49
Sabari <sabarihiswaran555@gmail.com>
2023-05-04 18:23:13 +0530

    interface vs types alias completed

--- 02Basics/myInterface.js
+++ 02Basics/myInterface.js
@@ -4,6 +4,16 @@ var sabari = {
     dbid: "1",
     name: "sabari",
     emailid: "sab@gmail.com",
+    githubtoken: "",
     startTrail: function () { return "Hello"; },
     getNumber: function (sc, off) { return 100; } //Here the parameter name can be different than what we declare in interface
+};
+var user2 = {
+    dbid: "1",
+    name: "sabari",
+    emailid: "sab@gmail.com",
+    role: "admin",
+    githubtoken: "",
+    startTrail: function () { return "Hello"; },
+    getNumber: function (sc, off) { return 100; }
 };
--- 02Basics/myInterface.ts
+++ 02Basics/myInterface.ts
@@ -10,13 +10,35 @@ interface User {
     getNumber(couponname : string,value:number) : number
 }
 
+//We can reopen the interface and add new type but in type alias we cannot do that
+
+interface User {
+    githubtoken : string 
+}
+
+//using extend keyword we can now include all properties in user to admin whereas in type aliase we will extend using ampersand
+
+interface Admin extends User {
+    role : "admin" | "ta" | "learner"
+}
 
 let sabari : User ={
     dbid : "1",
     name : "sabari",
     emailid: "sab@gmail.com",
+    githubtoken : "",
     startTrail : () => "Hello",
     getNumber : (sc:"sabari10",off:10) => 100      //Here the parameter name can be different than what we declare in interface
+}
+
+let user2 : Admin ={
+    dbid : "1",
+    name : "sabari",
+    emailid: "sab@gmail.com",
+    role: "admin",
+    githubtoken : "",
+    startTrail : () => "Hello",
+    getNumber : (sc:"sabari10",off:10) => 100 
 }
 
 export {}
