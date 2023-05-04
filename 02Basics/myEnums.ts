8b689be34a0c710e1196726b0f7baee3c2080f2d
Sabari <sabarihiswaran555@gmail.com>
2023-05-04 10:04:30 +0530

    enums completed

---
+++ 02Basics/myEnums.ts
@@ -0,0 +1,39 @@
+
+
+// Enumns are used when there can be restriction to the values like below,
+
+enum SeatAllocation {
+    //by default these below have values of 0,1,2 and goes on but we can also give customized value
+    aisle = "aisle",
+    middle = 3,
+    window
+}
+
+// The above enum code will generate below large js code
+
+// var SeatAllocation;
+// (function (SeatAllocation) {
+//     //by default these below have values of 0,1,2 and goes on but we can also give customized value
+//     SeatAllocation["aisle"] = "aisle";
+//     SeatAllocation[SeatAllocation["middle"] = 3] = "middle";
+//     SeatAllocation[SeatAllocation["window"] = 4] = "window";
+// })(SeatAllocation || (SeatAllocation = {}));
+// let seat = SeatAllocation.aisle;
+
+//But once we add const to enums it will generate less number of js code as below
+
+// const enum SeatAllocation {
+//     //by default these below have values of 0,1,2 and goes on but we can also give customized value
+//     aisle = "aisle",
+//     middle = 3,
+//     window
+// }
+
+
+// JS code
+// let seat = "aisle" /* SeatAllocation.aisle */;
+
+
+let seat = SeatAllocation.aisle
+
+export {}
