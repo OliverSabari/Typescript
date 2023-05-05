5c86ede5a61167e72e9588f284d7e01e8c4549b6
Sabari <sabarihiswaran555@gmail.com>
2023-05-05 20:57:33 +0530

    class shortcut

--- 03Purets/dist/index.js
+++ 03Purets/dist/index.js
@@ -9,3 +9,12 @@ class User {
 const sabari = new User(1, "sabari@gmail.com");
 // sabari.email = "sab@gmail"  email cannot be accessed outside the class as is private
 // sabari.city = "chennai"   this is not possible as city is readyonly
+//SHORTCUT FOR WRITING CLASS CONSTRUCTOR WHICH IS USED BY MANY PEOPLE
+//The below code will generate that same as above user class
+class User2 {
+    constructor(userid2, email2) {
+        this.userid2 = userid2;
+        this.email2 = email2;
+        this.city = "chennai";
+    }
+}
--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -20,3 +20,23 @@ const sabari = new User(1,"sabari@gmail.com")
 // sabari.email = "sab@gmail"  email cannot be accessed outside the class as is private
 
 // sabari.city = "chennai"   this is not possible as city is readyonly
+
+
+//SHORTCUT FOR WRITING CLASS CONSTRUCTOR WHICH IS USED BY MANY PEOPLE
+
+//The below code will generate that same as above user class
+
+class User2 {
+  
+    readonly city : string = "chennai"
+    constructor(
+        public userid2:number , 
+        public email2 : string
+        ) 
+        {
+  
+         }
+}
+
+
+
