3df8065f6a08d7932333a3ef649400938a5948e9
Sabari <sabarihiswaran555@gmail.com>
2023-05-05 20:53:04 +0530

    class, public and private completed

--- 03Purets/dist/index.js
+++ 03Purets/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("typescript is still here");
+class User {
+    constructor(userid, email) {
+        this.city = "Jaipur"; //Just a default readonly value
+        this.userid = userid,
+            this.email = email;
+    }
+}
+const sabari = new User(1, "sabari@gmail.com");
+// sabari.email = "sab@gmail"  email cannot be accessed outside the class as is private
+// sabari.city = "chennai"   this is not possible as city is readyonly
--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -1,2 +1,22 @@
 
-console.log("typescript is still here")
+class User {
+    //Below should be declared in order to assign the values in constructor using "this"
+   
+    public userid : number    //public keyword is by default for every property, it can be accessed everywhere
+    
+    private email : string     //Private property cannot be accessed outside the class
+
+    readonly city : string = "Jaipur"  //Just a default readonly value
+
+    constructor(userid:number , email : string) {
+        this.userid = userid,
+        this.email = email
+    }
+}
+
+
+const sabari = new User(1,"sabari@gmail.com")
+
+// sabari.email = "sab@gmail"  email cannot be accessed outside the class as is private
+
+// sabari.city = "chennai"   this is not possible as city is readyonly
